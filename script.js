c4760673458b3d1adbbd2a3d3e7674f2b0283d3d
subhashraaj <150801940+subhashraaj@users.noreply.github.com>
2024-01-17 10:21:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 const version = document.querySelector("#browser-info");
-version.innerText ="You are using "+ "Google " + navigator.appVersion;
+version.innerText ="You are using "+ "Google " + "version " + navigator.appVersion;
